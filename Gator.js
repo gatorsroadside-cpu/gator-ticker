dd609168fbf0c14b43eb1e050db302b82171a1fc
gatorsroadside-cpu <gatorsroadside@gmail.com>
2026-09-13 05:46:12 -0400

    Update ticker with current Warzone and multiplayer ranks

--- Gator.js
+++ Gator.js
@@ -1,6 +1,7 @@
 window.GATOR_LINES = [
   "Welcome to the Swamp",
   "Chomp! Chomp!",
-  "Crimson Ranked Player",
+  "Crimson WZ Ranked Player",
+  "Diamond MP Ranked Player",
   "Hit That Follow, I Follow Back!"
 ];
